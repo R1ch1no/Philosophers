64490408f6170a54b6c8b7a6e7232f45a1dc6800
Richard Kurnava <103361488+R1ch1no@users.noreply.github.com>
2023-05-14 15:01:23 +0200

    final changes

---
+++ philo/utils_three.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_three.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 11:34:15 by rkurnava          #+#    #+#             */
+/*   Updated: 2023/05/14 14:44:26 by rkurnava         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	ft_will_die(t_stats *stats, long long pos)
+{
+	pthread_mutex_lock(&stats->wait);
+	if (ft_timestamp() - stats->philo[pos].last_ate > stats->time_to_die
+		&& (stats->philo[pos].nb_ate <= stats->to_eat || stats->to_eat == -1))
+	{
+		pthread_mutex_unlock(&stats->wait);
+		ft_printer("died", stats, pos);
+		return (1);
+	}
+	else
+		pthread_mutex_unlock(&stats->wait);
+	return (0);
+}
+
+void	ft_printer(char *message, t_stats *stats, long long pos)
+{
+	pthread_mutex_lock(&stats->dead);
+	pthread_mutex_lock(&stats->print);
+	if (stats->death == 1)
+	{
+		pthread_mutex_unlock(&stats->dead);
+		pthread_mutex_unlock(&stats->print);
+		return ;
+	}
+	pthread_mutex_unlock(&stats->dead);
+	printf("%lli\t%lli\t%s\n", ft_timestamp() - stats->philo[pos].start_time,
+		pos + 1, message);
+	pthread_mutex_unlock(&stats->print);
+}
+
+void	ft_unfork(t_philosph *philo)
+{
+	pthread_mutex_unlock(&philo->rules->eat[philo->left_fork]);
+	pthread_mutex_unlock(&philo->rules->eat[philo->right_fork]);
+}
+
+void	phil_init_two(t_stats *stats)
+{
+	long	pos;
+
+	pos = -1;
+	while (++pos < stats->nb_philosoph)
+	{
+		stats->philo[pos].position = pos;
+		stats->philo[pos].nb_ate = 0;
+		stats->philo[pos].slept = 0;
+		stats->philo[pos].think = 0;
+		stats->philo[pos].rules = stats;
+		stats->philo[pos].left_fork = pos;
+		stats->philo[pos].right_fork = (pos + 1) % stats->nb_philosoph;
+	}
+}
---
+++ philo/utils_two.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_two.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 17:08:24 by rkurnava          #+#    #+#             */
+/*   Updated: 2023/05/14 13:15:30 by rkurnava         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	mutex_init_part_two(t_stats *stats)
+{
+	long long	pos;
+
+	pos = -1;
+	while (++pos < stats->nb_philosoph)
+	{
+		if (pthread_mutex_init(&stats->eat[pos], NULL) != 0)
+		{
+			pthread_mutex_destroy(&stats->wait);
+			pthread_mutex_destroy(&stats->count);
+			pthread_mutex_destroy(&stats->dead);
+			pthread_mutex_destroy(&stats->print);
+			while (--pos > 0)
+				pthread_mutex_destroy(&stats->eat[pos]);
+			return (write(2, "Mutex init error !\n", 20) && 1);
+		}
+	}
+	return (0);
+}
+
+int	ft_done_eating(t_stats *stats, long long pos)
+{
+	long long	pas;
+	long long	ate;
+
+	pas = -1;
+	ate = 0;
+	(void)pos;
+	if (stats->to_eat == -1)
+		return (0);
+	while (++pas < stats->nb_philosoph)
+	{
+		pthread_mutex_lock(&stats->count);
+		if (stats->philo[pas].nb_ate >= stats->to_eat)
+			++ate;
+		pthread_mutex_unlock(&stats->count);
+	}
+	if (ate == stats->nb_philosoph)
+		return (1);
+	return (0);
+}
+
+int	ft_sleep(t_philosph *philo)
+{
+	if (ft_done_eating(philo->rules, 1) == 1)
+		return (1);
+	pthread_mutex_lock(&philo->rules->dead);
+	if (philo->rules->death == 1)
+	{
+		pthread_mutex_unlock(&philo->rules->dead);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->rules->dead);
+	if (philo->slept == 0 && (philo->nb_ate <= philo->rules->to_eat
+			|| philo->rules->to_eat == -1))
+	{
+		philo->slept = 1;
+		ft_printer("is sleeping", philo->rules, philo->position);
+	}
+	ft_usleep(philo->rules->time_to_sleep * 1000, philo);
+	return (0);
+}
+
+void	sleep_think(t_philosph *philo)
+{
+	if (ft_sleep(philo) == 1)
+		return ;
+	pthread_mutex_lock(&philo->rules->dead);
+	if (philo->rules->death == 1)
+	{
+		pthread_mutex_unlock(&philo->rules->dead);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->rules->dead);
+	if (ft_done_eating(philo->rules, 1) == 1)
+		return ;
+	if (philo->think == 0 && (philo->nb_ate <= philo->rules->to_eat
+			|| philo->rules->to_eat == -1))
+	{
+		philo->think = 1;
+		ft_printer("is thinking", philo->rules, philo->position);
+	}
+}
+
+int	ft_check_params(int argc, char **argv)
+{
+	if (check_nums(argv) == 1)
+		return (1);
+	if (argc < 5 || argc > 6)
+		return (write(2, "Wrong number of arguments\n", 26) && 1);
+	if (argc == 6)
+	{
+		if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 500
+			|| ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0
+			|| ft_atoi(argv[4]) < 0 || ft_atoi(argv[5]) <= 0)
+			return (write(2, "Wrong parameters !\n", 19) && 1);
+	}
+	if (argc == 5)
+	{
+		if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 500
+			|| ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0
+			|| ft_atoi(argv[4]) < 0)
+			return (write(2, "Wrong parameters !\n", 19) && 1);
+	}
+	return (0);
+}
